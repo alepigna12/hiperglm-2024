89ecba7567ea87f0e57a830b31766e1789e6ea99
obsidian <obsidian@jhu.edu>
2024-08-14 09:08:25 -0400

    Adding tests for subset matvec Rcpp implementations

---
+++ tests/testthat/test-subset-matvec.R
@@ -0,0 +1,28 @@
+test_that("Effficient Rcpp implementation of row subset matvec coincides with base R result", {
+  set.seed(111)
+  
+  n <- 10^4; m <- 10; subset_frac = 0.1
+  A <- matrix(rnorm(n * m), n, m)
+  b <- rnorm(m)
+  row_index <- sample.int(n, subset_frac * n)
+  
+  expect_true(are_all_close(
+    A[row_index, ] %*% b,
+    row_subset_matvec_via_transpose(t(A), b, row_index)
+  ))
+})
+
+
+test_that("Effficient Rcpp implementation of col subset matvec coincides with base R result", {
+  set.seed(111)
+  
+  n <- 10^2; m <- 10^3; subset_frac = 0.1
+  A <- matrix(rnorm(n * m), n, m)
+  b <- rnorm(subset_frac * m)
+  col_index <- sample.int(m, subset_frac * m)
+  
+  expect_true(are_all_close(
+    A[, col_index] %*% b,
+    col_subset_matvec(A, b, col_index)
+  ))
+})
